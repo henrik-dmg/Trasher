0e8d675244b4ffc7df7fda239fbc6a9710b70086
Henrik Panhans <henrik@panhans.dev>
2023-05-23 11:47:07 +0200

    initial commit
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Package.swift
+++ Package.swift
@@ -7,22 +7,21 @@ let package = Package(
     name: "Trasher",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Trasher",
-            targets: ["Trasher"]),
+        .executable(
+            name: "trasher",
+            targets: ["Trasher"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "Trasher",
-            dependencies: []),
-        .testTarget(
-            name: "TrasherTests",
-            dependencies: ["Trasher"]),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+        ),
     ]
 )
--- Sources/Trasher/Trasher.swift
+++ Sources/Trasher/Trasher.swift
@@ -1,6 +1,57 @@
-public struct Trasher {
-    public private(set) var text = "Hello, World!"
+import ArgumentParser
+import Foundation
 
-    public init() {
+@main
+struct Trasher: ParsableCommand {
+
+    @Argument(help: "The file URL we want.", transform: URL.init(fileURLWithPath:))
+    var file: URL
+
+    mutating func validate() throws {
+        // Verify the file actually exists.
+        guard FileManager.default.fileExists(atPath: file.path) else {
+            throw ValidationError("File does not exist at \(file.path)")
+        }
     }
+
+    @Flag(name: .shortAndLong)
+    var recursive = false
+
+    @Flag(name: .shortAndLong)
+    var force = false
+
+    func run() throws {
+        var isDirectory = ObjCBool(false)
+        guard FileManager.default.fileExists(atPath: file.path, isDirectory: &isDirectory) else {
+            throw ValidationError("File does not exist at \(file.path)")
+        }
+        if isDirectory.boolValue && !recursive {
+            print("Supplied path is a directory. To delete a directory, supply the --recursive or -r flag")
+            Darwin.exit(1)
+        } else {
+            if force {
+                try FileManager.default.removeItem(at: file)
+            } else {
+                moveFileToTrash()
+            }
+        }
+    }
+
+    private func moveFileToTrash() {
+        let myAppleScript = """
+        tell application "Finder"
+            move POSIX file "\(file.path)" to trash
+        end tell
+        """
+        guard let scriptObject = NSAppleScript(source: myAppleScript) else {
+            print("Could not launch AppleScript")
+            Darwin.exit(1)
+        }
+        var error: NSDictionary?
+        _ = scriptObject.executeAndReturnError(&error)
+        if let error {
+            print("error: \(error)")
+        }
+    }
+
 }
